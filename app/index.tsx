02cf9df504b41194c8848460661dd2218efe134c
Grian Gajila <griangajila2@gmail.com>
2025-06-01 22:48:53 +0800

    wip

--- app/index.tsx
+++ app/index.tsx
@@ -1,59 +1,25 @@
-import { icon } from "@/constant/icon";
 import { images } from "@/constant/images";
 import { useRouter } from "expo-router";
-import React, { useRef } from "react";
-import {
-	GestureResponderEvent,
-	Image,
-	PanResponder,
-	PanResponderGestureState,
-	Text,
-	View,
-} from "react-native";
+import React, { useEffect } from "react";
+import { Image, View } from "react-native";
 export default function Welcome() {
 	const router = useRouter();
 
-	const panResponder = useRef(
-		PanResponder.create({
-			onMoveShouldSetPanResponder: (
-				evt: GestureResponderEvent,
-				gestureState: PanResponderGestureState
-			) => {
-				return Math.abs(gestureState.dx) > 20 && Math.abs(gestureState.dy) < 20;
-			},
-			onPanResponderRelease: (evt, gestureState) => {
-				if (gestureState.dx < 20) {
-					router.push("/Home");
-				}
-			},
-		})
-	).current;
+	useEffect(() => {
+		const timer = setTimeout(() => {
+			router.replace("/Home");
+		}, 2000);
+
+		return () => clearTimeout(timer);
+	});
 
 	return (
-		<View
-			className="flex-1 items-center justify-center bg-background px-8"
-			{...panResponder.panHandlers}
-		>
-			<View className="flex-1 justify-between py-40 items-center">
-				<Text className="text-center text-wrap font-bold text-4xl text-white">
-					Welcome to Aquaflow
-				</Text>
-				<Image
-					className="w-[380px] h-[100px] bg-white  rounded-md"
-					source={images.Logo}
-					resizeMode="contain"
-				/>
-				<View>
-					<Image
-						tintColor={"white"}
-						source={icon.swipe}
-						className="h-20 w-20 self-center"
-					/>
-					<Text className="text-text font-semibold text-center text-xl">
-						Swipe right to continue
-					</Text>
-				</View>
-			</View>
+		<View className="flex-1 items-center justify-center bg-white px-8">
+			<Image
+				className="w-64 h-64  rounded-md"
+				source={images.Logo}
+				resizeMode="contain"
+			/>
 		</View>
 	);
 }
